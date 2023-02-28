b6ecea000fcdad12b0676c3edd4c8173d6719e4b
sanyuanzai <931502949@qq.com>
2023-02-28 21:46:11 +0800

    添加IdSelect组件

--- src/components/id-selcet.tsx
+++ src/components/id-selcet.tsx
@@ -1,7 +1,33 @@
+import { Select } from "antd";
 import React, { memo } from "react";
+import { Raw } from "types";
+type SelectProps = React.ComponentProps<typeof Select>;
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
 
-const IdSelect = memo(() => {
-  return <div>IdSelect</div>;
+const IdSelect = memo((props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.id} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
 });
-
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
 export default IdSelect;
